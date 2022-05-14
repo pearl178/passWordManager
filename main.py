eeccc7d44b244c8c4ca853d92db4984059368839
pearl178 <89487585+pearl178@users.noreply.github.com>
2022-05-14 17:37:45 -0600

    Change format

--- main.py
+++ main.py
@@ -34,16 +34,16 @@ def save():
             try:
                 with open("data.json", 'r') as data_file:
                     data = json.load(data_file)
-        except FileNotFoundError:
-            with open("data.json", 'w') as data_file:
-                json.dump(new_data, data_file, indent=4)
-        else:
-            data.update(new_data)
-            with open("data.json", 'w') as data_file:
-                json.dump(data, data_file, indent=4)
-        finally:
-            website_input.delete(0, END)
-            password_input.delete(0, END)
+            except FileNotFoundError:
+                with open("data.json", 'w') as data_file:
+                    json.dump(new_data, data_file, indent=4)
+            else:
+                data.update(new_data)
+                with open("data.json", 'w') as data_file:
+                    json.dump(data, data_file, indent=4)
+            finally:
+                website_input.delete(0, END)
+                password_input.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
